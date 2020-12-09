ba23ebc0e8fd28fdbb7ed20dd7860fe9955133b8
James-Ashley <jkbashley@gmail.com>
2020-12-09 09:35:31 -0500

    rendered html table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,17 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody")
+
+console.log(tableData);
+
+tableData.forEach((ufoSighting) => {
+    console.log(ufoSighting);
+    var row = tbody.append("tr")
+
+    Object.entries(ufoSighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    })
+
+});
